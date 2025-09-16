ea5a7d6e993206dca1925c2d29389d4bb22b3db3
Cotrim12 <cotrimdasilvafelippe908@gmail.com>
2025-09-16 11:57:58 -0300

    aula 16/09

---
+++ quicksort/ordenacao.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void TrocarElemento(int * A, int * B);
+int Particao(int * V, int Inf, int Sup);
+void QuickSort(int * V, int Inf, Int Sup);
+
+
+int main(){
+
+    int Vetor[] = {3, 6, 4, 5, 1, 7, 2};
+    int Tamanho = sizeof(Vetor) / sizeof(int);
+    Particao(Vetor, 0, Tamanho - 1);
+
+    int X = 25;
+    int Y = 37;
+    TrocarElemento(&X, &Y);
+    printf("%d %d", X, Y);
+
+}
+
+void TrocarElemento(int * A, int * B){
+
+// Obs: A e B são ponteiros (guardam endereço de memória)
+
+int temp = *A; // Temp recebe o valor apontado por A
+*A = *B; // O valor apontado por A passa a ser o valor apontado pro B
+*B = temp; // O valor apontado por B recebe temp(valor apontado)
+
+
+}
+
+int Particao(int * V, int Inf, int Sup){
+    int Pivot = V[(Inf + Sup) / 2];
+    int i = Inf;
+    int j = Sup;
+
+    while (i <= j){
+        while(V[i] < Pivot) i++;
+        while(V[j] < Pivot) j--;
+        if(i <= j){
+            TrocarElemento(&V[i], &V[j]);
+            i++;
+            j--;
+        }
+
+        
+    }
+
+    return i;
+
+}
+
+void QuickSort(int * V, int Inf, Int Sup){
+if(Inf < Sup);
+int P = Particao(V, Inf, Sup);
+QuickSort(V, Inf, P - 1);
+QuickSort(V, P, sup);
+
+}
